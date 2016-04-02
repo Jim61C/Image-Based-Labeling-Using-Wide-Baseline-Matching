791f62b6ebbcc37905ed8d814d3f6ea505580c7c
JimXing <xyf.jim@gmail.com>
2016-04-03 04:35:34 +0800

    move drawing of filtered patches up in findDistinguishablePatchesAlgo3

--- comparePatches.py
+++ comparePatches.py
@@ -984,10 +984,11 @@ def findDistinguishablePatchesAlgo3(img, sigma, remove_duplicate_thresh_dict , h
 	maxCornerResponse, cornerResponseMatrix = cornerResponse.getHarrisCornerResponse(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), sigma, step)
 	filtered_patches = cornerResponse.filter_patches(patches, harris_thresh_pass, cornerResponseMatrix, maxCornerResponse)
 	# positions = [(patch.x, patch.y) for patch in filtered_patches]
+	# drawPatchesOnImg(np.copy(img), filtered_patches)
 	print "len(filtered_patches):", len(filtered_patches)
 	for this_filtered_patch in filtered_patches:
 		this_filtered_patch.initializeFeatureObjects()
-	# drawPatchesOnImg(np.copy(img), filtered_pgatches)
+	
 	"""
 	2. Compute Combinatorial LDA score for each of the filtered patches (keep the set of best combination and its score + weights), remove from list if score too low
 	"""
